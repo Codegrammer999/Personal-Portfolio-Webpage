bde3b8e93725dc80ef9f841d011f9379694fe353
Coderr999 <shyyguyy501@gmail.com>
2024-09-26 00:43:52 +0100

    home page

---
+++ src/components/LinkBtn.vue
@@ -0,0 +1,15 @@
+<script setup>
+defineProps({
+    to: {
+        required: true
+    }
+})
+</script>
+
+<template>
+     <router-link 
+        :to="to"
+        class="bg-blue-500 px-6 py-3 rounded-lg font-medium shadow-md hover:bg-blue-600 focus:ring-2 ring-slate-200 transition duration-300">
+        <slot/>
+      </router-link>
+</template>
